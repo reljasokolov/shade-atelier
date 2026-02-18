9f6b18019e0a154211bd1323ce8200ce174779e3
reljasokolov <reljasokolov@gmail.com>
2026-02-18 19:04:56 +0200

    Build a responesive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold" hideBelow="lg">
+        aside
+      </GridItem>
+      <GridItem area="main" bg="blue">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
