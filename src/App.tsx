3f3e4d04e261cb8a3b8b045e120c01b5e7c031f6
reljasokolov <reljasokolov@gmail.com>
2026-02-23 16:10:14 +0200

    Adding Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/my/NavBar";
+import { ColorModeButton, DarkMode } from "../src/components/ui/color-mode";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar></NavBar>
-      </GridItem>
-      <GridItem area="aside" bg="gold" hideBelow="lg">
-        aside
-      </GridItem>
-      <GridItem area="main" bg="blue">
-        main
-      </GridItem>
-    </Grid>
+    <DarkMode>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar></NavBar>
+          <ColorModeButton
+            position="fixed"
+            top="15px"
+            right="10px"
+            zIndex="1000"
+          />
+        </GridItem>
+        <GridItem area="aside" bg="gold" hideBelow="lg">
+          aside
+        </GridItem>
+        <GridItem area="main" bg="blue">
+          main
+        </GridItem>
+      </Grid>
+    </DarkMode>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ColorModeProvider } from "./components/ui/color-mode";
+import { system } from "./theme";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <App />
+    <ChakraProvider value={system}>
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
     </ChakraProvider>
   </StrictMode>,
 );
