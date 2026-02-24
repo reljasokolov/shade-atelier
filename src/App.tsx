c9f76c7a771580c569c469e5b30d970fec84135b
reljasokolov <reljasokolov@gmail.com>
2026-02-24 17:10:44 +0200

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/my/NavBar";
-import { ColorModeButton, DarkMode } from "../src/components/ui/color-mode";
+import { ColorModeButton } from "../src/components/ui/color-mode";
 
 function App() {
   return (
-    <DarkMode>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`,
-        }}
-      >
-        <GridItem area="nav">
-          <NavBar></NavBar>
-          <ColorModeButton
-            position="fixed"
-            top="15px"
-            right="10px"
-            zIndex="1000"
-          />
-        </GridItem>
-        <GridItem area="aside" bg="gold" hideBelow="lg">
-          aside
-        </GridItem>
-        <GridItem area="main" bg="blue">
-          main
-        </GridItem>
-      </Grid>
-    </DarkMode>
+    <Grid
+      bg={"bg.canvas"}
+      minH="100vh"
+      templateRows="auto 1fr"
+      templateColumns={{ base: "1fr", lg: "250px 1fr" }}
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar></NavBar>
+        <ColorModeButton position="fixed" top="0px" right="0px" zIndex="1000" />
+      </GridItem>
+
+      <GridItem area="main">main</GridItem>
+    </Grid>
   );
 }
 
--- src/components/my/NavBar.tsx
+++ src/components/my/NavBar.tsx
@@ -1,12 +1,67 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../../assets/images.jpeg";
-const NavBar = () => {
-  return (
-    <HStack>
-      <Image src={logo} boxSize={"60px"} />
-      <Text>NavBar</Text>
-    </HStack>
-  );
-};
+import {
+  Box,
+  Flex,
+  HStack,
+  Text,
+  Link,
+  Button,
+  Icon,
+  Spacer,
+} from "@chakra-ui/react";
+import { LuPhone, LuMail, LuMapPin, LuClock } from "react-icons/lu";
 
-export default NavBar;
+export default function Navbar() {
+  return (
+    <Box position="sticky" top="0" zIndex="1000" bg="#d6ccc2" color="#2b2b2b">
+      {/* TOP INFO BAR */}
+      <Flex bg="#cbbfb3" px="8" py="2" justify="center">
+        <HStack fontSize="sm">
+          <HStack>
+            <Icon as={LuPhone} />
+            <Text>+BR TELEFONA</Text>
+          </HStack>
+
+          <HStack>
+            <Icon as={LuMail} />
+            <Text>EMAIL</Text>
+          </HStack>
+
+          <HStack>
+            <Icon as={LuMapPin} />
+            <Text>ADRESA</Text>
+          </HStack>
+
+          <HStack>
+            <Icon as={LuClock} />
+            <Text>RADNO VREME</Text>
+          </HStack>
+        </HStack>
+      </Flex>
+
+      {/* MAIN NAV */}
+      <Flex px="6" py="2" align="center">
+        {/* LOGO */}
+        <Text fontSize="xl" fontWeight="bold">
+          LOGO
+        </Text>
+
+        {/* LINKS */}
+        <HStack ml="4" wordSpacing={"4"}>
+          <Link>Home</Link>
+          <Link>Services</Link>
+          <Link>Shop</Link>
+          <Link>Team</Link>
+          <Link>Pricing</Link>
+          <Link>Contact</Link>
+        </HStack>
+
+        {/* PUSH CTA RIGHT */}
+        <Box flex="1" />
+        {/* CTA BUTTON */}
+        <Button bg="#a68a64" color="white" _hover={{ bg: "#927656" }}>
+          Book Now
+        </Button>
+      </Flex>
+    </Box>
+  );
+}
