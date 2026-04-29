77331b237f9cb1bfdef17db6aded7964c7a89ff4
reljasokolov <reljasokolov@gmail.com>
2026-04-29 22:27:36 +0300

    colormodeprovider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ColorModeProvider } from "./components/ui/color-mode";
 import { system } from "./theme";
 import "./index.css";
 import App from "./App.tsx";
@@ -9,9 +8,7 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider value={system}>
-      <ColorModeProvider>
-        <App />
-      </ColorModeProvider>
+      <App />
     </ChakraProvider>
   </StrictMode>,
 );
